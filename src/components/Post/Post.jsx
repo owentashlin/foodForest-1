c992fef520bf0ee83d8969f0305589126f9b2780
mzecic <matejzecic@gmail.com>
2023-02-10 17:26:12 -0500

    co-authors: owentashlin, tschmiedl

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,11 +1,33 @@
+import "./Post.css";
+
 export default function Post({ post }) {
   return (
-    <div>
-      <p>{post.title}</p>
-      <p>{post.content}</p>
-      <p>{post.price}</p>
-      <p>{post.contactInfo}</p>
-      <img src={post.imageUrl} alt="" />
-    </div>
+    <>
+      <div>
+        <div className="post-container">
+          <p>{post.title}</p>
+          <p>{post.content}</p>
+          <p>{post.price}</p>
+          <p>{post.contactInfo}</p>
+        </div>
+        <div className="image-container">
+          <img
+            style={{ margin: "10px" }}
+            width="170px"
+            height="140px"
+            src={post.imageUrl}
+            alt=""
+          />
+          <img
+            style={{ margin: "10px" }}
+            width="170px"
+            height="140px"
+            src={post.imageUrl}
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="separator"></div>
+    </>
   );
 }
